9c4093e90a31371e0183ae21e16f606e71acaf45
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-27 16:15:01 +0100

    Flip player on moving left and right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,7 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Run();
+        FlipSprite();
 	}
 
     private void Run()
@@ -29,5 +30,17 @@ public class Player : MonoBehaviour {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(
+                Mathf.Sign(myRigidBody.velocity.x) * Mathf.Abs(transform.localScale.x),
+                transform.localScale.y
+                );
+        }
     }
 }
