ac1fc7150e6e26a21a5d096b2bd5e7ae7b218d04
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-03-04 11:31:04 +0100

    Remember coins that are picked up in a level (requirement: player lives > 0)

---
+++ Assets/ScenePersist.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour {
+
+    int startIndex = -1;
+
+    private void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        startIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void Update()
+    {
+        int currrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (startIndex != currrentSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
